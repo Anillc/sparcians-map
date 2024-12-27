dbf642f1c8e8d86f3abdb28ba2c731fa4d4bb5fc
Anillc <i@anillc.cn>
2024-12-27 17:28:31 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  outputs = inputs@{
+    self, nixpkgs, flake-parts,
+  }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    perSystem = { pkgs, self', ... }: {
+      packages.sparta = pkgs.stdenv.mkDerivation rec {
+        pname = "sparta";
+        version = "map_v2.0.20";
+        src = "${./.}/sparta";
+        cmakeFlags = [
+          "-DGIT_REPO_VERSION=${version}"
+          "-DCMAKE_BUILD_TYPE=Release"
+        ];
+        buildInputs = with pkgs; [ boost yaml-cpp rapidjson sqlite zlib hdf5 ];
+        nativeBuildInputs = with pkgs; [ cmake cppcheck ];
+      };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ self'.packages.sparta ];
+      };
+    };
+  };
+}
